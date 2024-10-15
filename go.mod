c6080b92fcf2362b03793c4ec0b0e834f2e94ea1
Einenlum <contact@einenlum.com>
2024-10-15 02:20:41 +0200

    Add cobra cli

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.19
 
 require (
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"einenlum/edicon/internal/plugins/ini"
-	"fmt"
-	"os"
-)
+import "einenlum/edicon/cmd"
 
 func main() {
-	if len(os.Args) < 5 {
-		fmt.Println("Usage: edicon <file-path> set <key> <value>")
-
-		return
-	}
-
-	argsWithoutProg := os.Args[1:]
-	filePath := argsWithoutProg[0]
-	// action := argsWithoutProg[1]
-	key := argsWithoutProg[2]
-	value := argsWithoutProg[3]
-
-	iniFile, err := ini.EditIniFile(filePath, key, value)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
-	}
-
-	fmt.Println(ini.OutputIniFile(iniFile, ini.FullOutput))
+	cmd.Execute()
 }
